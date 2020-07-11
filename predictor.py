ac2d87288d44c1245149b4beb056da7908a37f9c
Valentina <feruereru@gmail.com>
2020-07-11 17:23:10 -0500

    Comented

--- predictor.py
+++ predictor.py
@@ -2,13 +2,20 @@ import pandas as pd
 import numpy as np
 import gensim
 import sys
+from tensorflow.keras.layers import Dense
+from tensorflow.keras.models import Sequential
 
-n_gram = 2
-dimensions = 100
-epochs = 100
+n_gram = 2  # Number of considered words for the prediction
+dimensions = 100  # Number of dimensions for the word vector model
+epochs = 100  # Number of epochs for the neural network
+
+#   Creating the word vector model (just once)
+
+#   Loading the trained model
+model = gensim.models.Word2Vec.load("trained_model")
 
 f = open("file", "r+")
-sentences = f.readlines()
+sentences = f.readlines()   # Sentences for training the prediction and creating the word vector model
 
 csv_data = {
     'current': [],
@@ -16,9 +23,10 @@ csv_data = {
 }
 index = 0
 print("Loading sentences...")
+# Creation of the dataset for training the neural network
 for line in sentences:
     index += 1
-    line = 'y '+line
+    line = 'y ' + line
     words = [elem if elem else None for elem in line.split()]
     for i in range(len(words) - n_gram):
         current = ''.join(words[i + j] + ' ' for j in range(n_gram))[:-1]
@@ -26,13 +34,11 @@ for line in sentences:
         csv_data['current'].append(current)
         csv_data['next'].append(next_word)
 
-# Data to Pandas data frame
-print("Reading data from dictionary")
+# Loading into a Pandas datagram
+print("Reading data from dictionary...")
 df = pd.DataFrame(csv_data, columns=['current', 'next'])
 
-model = gensim.models.Word2Vec.load("trained_model")
-
-print("Preparing data for model")
+print("Preparing data for model...")
 current_inputs = []
 next_inputs = []
 for index, row in df.iterrows():
@@ -59,6 +65,7 @@ nn.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy
 xs = np.array(current_inputs)
 ys = np.array(next_inputs)
 print("%d epochs..." % epochs)
+# Training the neural network model
 history = nn.fit(xs, ys, epochs=epochs, batch_size=10)
 print(history)
 model_json = nn.to_json()
@@ -69,18 +76,20 @@ with open('nn.json', "w") as json_file:
 
 nn.save_weights('nn.h5')
 
+# Predict function
 def predict(sentence):
     sentence = sentence.lower()
     words_in_sentence = [elem if elem else None for elem in sentence.split()]
     if len(words_in_sentence) < n_gram:
-        sentence = 'y '+sentence
-    # TODO: Check if number of words is smaller than n_gram
+        sentence = 'y ' + sentence
     words_in_sentence = [elem if elem else None for elem in sentence.split()]
     last_words = words_in_sentence[-n_gram:]
     last_words_vector_list = []
     for word in last_words:
         last_words_vector_list.append(model.wv.get_vector(word))
+    # Sentences must be a np array
     last_words_vector = np.array(last_words_vector_list).flatten()
     array_to_predict = np.array([last_words_vector])
     predicted_vector = nn.predict(array_to_predict, verbose=0)
     predicted_words = model.wv.similar_by_vector(predicted_vector[0], restrict_vocab=900)
+    return predicted_words
